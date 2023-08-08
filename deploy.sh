03199d20d270f8e08da1a3e4ef251927c0ddfb09
Fariz <fariz@devsign.neo.internal>
2023-08-08 18:10:05 +0700

    prevent permission denied while exec build.sh

--- deploy.sh
+++ deploy.sh
@@ -1,10 +1,12 @@
 #!/bin/sh
-docker compose down && 
-git pull && 
-cd frontend && 
-npm install && 
-npm run build && 
-rm -rf node_modules && 
-cd ../ && 
-docker compose build && 
+git reset --hard &&
+docker compose down &&
+git pull &&
+chmod +x deploy.sh &&
+cd frontend &&
+npm install &&
+npm run build &&
+rm -rf node_modules &&
+cd ../ &&
+docker compose build &&
 docker compose up -d
